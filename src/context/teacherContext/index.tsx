5280522273dc822c4e9ae18f12bc473cd83b70a3
hagleyson <hagleyson@gmail.com>
2023-04-07 13:00:53 -0300

    🚧 adding methods in context for teachers

--- src/context/teacherContext/index.tsx
+++ src/context/teacherContext/index.tsx
@@ -4,12 +4,17 @@ import { ITeacherContext } from "./interface";
 import { toast } from "react-toastify";
 
 import { teacherServices } from "@/shared/services";
-import { IMeta, ITeacher } from "@/shared/Interfaces";
+import { ICreateOrUpdateTeacher, IMeta, ITeacher } from "@/shared/Interfaces";
 
 export const Teacher = createContext({} as ITeacherContext);
 
 export function TeacherProvider({ children }: { children: React.ReactNode }) {
-  const { listAll: listAllService } = teacherServices();
+  const {
+    listAll: listAllService,
+    register: registerService,
+    update: updateService,
+    deleteTeacher: deleteService,
+  } = teacherServices();
   const [meta, setMeta] = useState<IMeta>({} as IMeta);
   const [teachers, setTeachers] = useState<ITeacher[]>([]);
   async function listAll(page: number) {
@@ -23,6 +28,16 @@ export function TeacherProvider({ children }: { children: React.ReactNode }) {
       if (data.data) {
         setMeta(data.meta);
         setTeachers(data.data);
+      }
+    } catch ({ error }: any) {
+      toast.error("Ocorreu um ao listar professores");
+    }
+  }
+  async function register(dataTeacher: ICreateOrUpdateTeacher) {
+    try {
+      const response = await registerService(dataTeacher);
+      if (response.status === 200) {
+        toast.success("Professor Criado com sucesso!");
       }
     } catch ({ error }: any) {
       toast.error("Ocorreu um ao listar professores");
